13a273f34dbca1958b16bd95c8f35557d38428b5
Ermelindo Bernardo <bernardotooroffice@gmail.com>
2020-06-07 13:25:50 +0100

    RenderRepositoryEstatica
    Renderizar elementos de forma estática

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 class App{
     constructor(){
         this.repositories = [];
+
         this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
+
         this.registerHandlers();
     }
 
@@ -19,8 +22,37 @@ class App{
             html_url:'https://github.com/Rocketseat'
         });
 
-        console.log(this.repositories);
+        this.render();
     }
+    render(){
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avantar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+            
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
+    }
+
 }
 
 new App();
+
+console.log('ola');
